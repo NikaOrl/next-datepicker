0aeee5323beffdf31c101c03a540505194cb9031
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-03-14 17:17:34 +0300

    webpack fixed

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,4 @@
 module.exports = (storybookBaseConfig, configType, defaultConfig) => {
-  defaultConfig.output.publicPath = configType === 'PRODUCTION' ? '/next-checkbox/' : '/';
+  defaultConfig.output.publicPath = configType === 'PRODUCTION' ? '/next-datepicker/' : '/';
   return defaultConfig;
 };
